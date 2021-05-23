bb2a8541964ba74b71164d5a8d0a8cef3c964c7a
S4TURN0 <wesleysampaio66@gmail.com>
2021-05-23 04:28:51 -0300

    :sparkles: - Adicionada funcionalidade para descoberta de subdominios e ajustes realizados no web fuzzing

--- atomic-python.py
+++ atomic-python.py
@@ -3,13 +3,36 @@ import socket
 import argparse
 import requests
 
-
 parse = argparse.ArgumentParser(description="Esse código serve para realizar um portscan")
 parse.add_argument('-d','--dest',required=True,type=str,dest="destino",help="Insira o endereço de destino")
 parse.add_argument('-p',help="Insira a porta para ser escaneada")
+parse.add_argument('-s','--subs',action="store_true",dest="subdomain",help="Realizar discoberta de subdominios")
+parse.add_argument('-ps','--scan',action="store_true",dest="portscan",help="Realizar escaneamento de portas")
 parse.add_argument('-f','--fuzz',action="store_true",dest="fuzzing",help="Realizar um fuzzing ao encontrar portas web abertas")
+parse.add_argument('-hc',help="Para especificar os códigos que deseja esconder")
+parse.add_argument('-sc',help="Para especificar os códigos que deseja mostrar")
 parse.add_argument('-w',type=str,help="Adicionar uma wordlist para a realização do fuzzing (OPCIONAL)")
 args = parse.parse_args()
+
+# Função para realizar a descoberta de subdominios
+def subdomain(domain):
+    print("\n[+] Iniciando a descoberta de subdominios\n")
+
+    url = 'https://dns.bufferover.run/dns?q='+domain
+    req = requests.get(url)
+    subs = []
+
+    for x in req.json()['FDNS_A']:
+        
+        domains = x.split(',')
+        subs.append(domains[1])
+
+    # Remover subdominios duplicados
+    uniq_subs = list(dict.fromkeys(subs))
+
+    for x in uniq_subs: print(x)
+
+    print("\n[+] ",len(uniq_subs)," Sub-dominios encontrados!")
 
 # Função para realizar o portscan
 def port_scan(ports):
@@ -42,51 +65,77 @@ def port_scan(ports):
 def fuzzing(ports):
     print('\n[+] Iniciando o Web Fuzzing\n')
 
-    if 443 in ports:
-        with open('wordlists/common.txt','r') as words:
-            for word in words.readlines():
-                url = "https://{}/{}".format(domain,word)
-                req = requests.get(url)
-                print("[+] Status: {} Wordlist: {}  ".format(req.status_code,word.rstrip("\n")))
-
-    elif 80 in ports:
-        with open('wordlists/common.txt','r') as words:
-            for word in words.readlines():
-                url = "http://{}/{}".format(domain,word)
-                req = requests.get(url)
-                print("[+] Status: {} Wordlist: {} ".format(req.status_code,word.rstrip("\n")))
+    if args.w != None:
+        wordlist(args.w)
+        
+    else:
+        wordlist('wordlists/common.txt')
 
 # Função para gerenciamento de wordlists
-def wordlist():
-    words = open(args.w,'r')
-    for word in words.readlines():
-        pass
+def wordlist(arquivo):
+    with open(arquivo,'r') as words: 
+        for word in words.readlines():
+            if 443 in ports:
+                url = "https://{}/{}".format(domain,word)
+
+            elif 80 in ports:
+                url = "http://{}/{}".format(domain,word)
+
+            req = requests.get(url)
+
+            if args.sc != None:
+                if ',' in args.sc:
+                    x = args.sc.split(",")
+                    status = [ int(p) for p in x]
+                   
+                    if req.status_code in status:    
+                        print("[+] Status: {} Wordlist: {}  ".format(req.status_code,word.rstrip("\n")))
+                else:
+                    if req.status_code == int(args.sc):    
+                        print("[+] Status: {} Wordlist: {}  ".format(req.status_code,word.rstrip("\n")))
+
+            elif args.hc != None:
+                if ',' in args.hc:
+                    x = args.hc.split(",")
+                    status = [ int(p) for p in x]
+                   
+                    if not req.status_code in status:    
+                        print("[+] Status: {} Wordlist: {}  ".format(req.status_code,word.rstrip("\n")))
+                else:
+                    if req.status_code != int(args.hc):    
+                        print("[+] Status: {} Wordlist: {}  ".format(req.status_code,word.rstrip("\n")))
+            else:
+                print("[+] Status: {} Wordlist: {}  ".format(req.status_code,word.rstrip("\n")))
 
 try:
     domain = args.destino
     ip = socket.gethostbyname(domain)
     ports = args.p
 
-    if ports == None:        
-        ports = []
-        for x in range(0,1024):
-            ports.append(x)
-        port_scan(ports)
+    if args.subdomain == True:
+        subdomain(domain)
 
-    elif '-' in ports:
-        start_port, end_port = ports.split("-")
-        start_port, end_port = int(start_port), int(end_port)
-        ports = [ p for p in range(start_port, end_port+1)]
-        port_scan(ports)
+    if args.portscan == True:
+        if ports == None:        
+            ports = []
+            for x in range(0,1024):
+                ports.append(x)
+            port_scan(ports)
 
-    elif ',' in ports:
-        x = ports.split(",")
-        ports = [ int(p) for p in x]
-        port_scan(ports)
-    
-    else:
-        ports = [int(ports)]
-        port_scan(ports)
+        elif '-' in ports:
+            start_port, end_port = ports.split("-")
+            start_port, end_port = int(start_port), int(end_port)
+            ports = [ p for p in range(start_port, end_port+1)]
+            port_scan(ports)
+
+        elif ',' in ports:
+            x = ports.split(",")
+            ports = [ int(p) for p in x]
+            port_scan(ports)
+        
+        else:
+            ports = [int(ports)]
+            port_scan(ports)
 
 except socket.gaierror:
     print("\n[-] Não foi possivel se conectar ao servidor")
