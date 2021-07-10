0ae03059b19b4aabf7fc5fba3866e491a4386bda
S4TURN0 <wesleysampaio66@gmail.com>
2021-07-10 03:30:21 -0300

    Melhorias na descoberta de subdominios
    Features: Consultas por dominios que utilizam IPV6
    
    Features: Integração com Shodan, Threatcrowd e alienvault
    
    Bug Fix: Correção no parsing do passive total

--- scripts/subdomains.py
+++ scripts/subdomains.py
@@ -1,60 +1,76 @@
-import requests,re,json,time
-from dns import resolver
-from dns.exception import DNSException
+import requests,re,time
+from dns import *
 from config.keys import *
 
 # Variaveis globais
-global subs
 subs = []
 active_subs = []
 
 # Função para realizar a descoberta de subdominios
-def subdomain(domain):
+def subdomain(args):
     print("\n[*] Iniciando a descoberta de subdominios\n\n")
 
-    anubis(domain)    
-    bufferover(domain)
-    crt(domain)
-    certspotter(domain)
-    dnsdb(domain)
-    riddler(domain)
-    hackertarget(domain)
-    passivetotal(domain)
-    securitytrails(domain)
-    virustotal(domain)
-    
+    global domain
+    domain = args
+
+    alienvault()
+    anubis()    
+    bufferover()
+    crt()
+    certspotter()
+    dnsdb()
+    riddler()
+    hackertarget()
+    passivetotal()
+    securitytrails()
+    threatcrowd()
+    virustotal()
+    shodan()
+
     print('\n\n[*] Removendo subdominios duplicados')
+
     uniq_subs = list(dict.fromkeys(subs))
     uniq_subs.sort()
-
-    print('\n[+] {} Subdominios encontrados!'.format(len(uniq_subs)))
+    
+    print('\n[+] {} Subdominios únicos encontrados!'.format(len(uniq_subs)))
 
     print('\n[*] Filtrando por subdominios ativos\n')
 
-    for x in uniq_subs: 
-        subdomain_check(x,'A')
+    for x in uniq_subs:
+        subdomain_check(x)
 
     print("\n[+] {} Subdominios Ativos!\n".format(len(active_subs)))
-    return uniq_subs
+    return active_subs
 
-def subdomain_check(sub,query):
+def subdomain_check(sub):
     try:
-        conn = resolver.query(sub,query)
+        conn = resolver.query(sub,'a')
         print(conn.qname)
         active_subs.append(conn.qname)
-    except DNSException as e:
-        #print('Error DNS: {}'.format(e))
-        pass
+    except resolver.NoAnswer as e:
+        try:
+            conn = resolver.query(sub,'aaaa')
+            print(conn.qname)
+            active_subs.append(conn.qname)
+        except:
+            return
+    except:
+        return
 
-def certspotter(domain):
-    print("[+] Consultando certspotter")
+def certspotter():
+
     params = (
             ('domain', domain),
             ('include_subdomains', 'true'),
             ('expand', 'dns_names'),
         )
-    # Insira sua chave de API em Bearer
-    header = {"Authorization": "Bearer "+cert_api()}
+    # Insira sua chave de API após o Bearer
+    try:
+        api = cert_api()
+        header = {"Authorization": "Bearer "+api}
+        print("[+] Consultando certspotter")
+    except: return
+    
     response = requests.get('https://api.certspotter.com/v1/issuances',headers=header, params=params).json()
     #print('\nCertPotter: {}'.format(response))
     for a in response:
@@ -66,9 +82,9 @@ def certspotter(domain):
                     if b != None:
                         subs.append(b.group(0))
         except:
-            pass
+            return
 
-def bufferover(domain):
+def bufferover():
     print("[+] Consultando bufferover")
     sub = 'https://dns.bufferover.run/dns?q='+domain
     req = requests.get(sub).json()['FDNS_A']
@@ -78,7 +94,7 @@ def bufferover(domain):
             if re.search('.*\.'+domain+'$',domains):
                 subs.append(domains)
 
-def riddler(domain):
+def riddler():
     print("[+] Consultando riddler")
     url = 'https://riddler.io/search/exportcsv?q=pld:'+domain
     response = requests.get(url)
@@ -86,7 +102,7 @@ def riddler(domain):
     for x in sub:
         subs.append(x)
 
-def anubis(domain):
+def anubis():
     print("[+] Consultando anubis")
     url = 'https://jldc.me/anubis/subdomains/'+domain
     response = requests.get(url)
@@ -96,7 +112,7 @@ def anubis(domain):
         if x != b'[]':
             subs.append(x)
 
-def crt(domain):
+def crt():
     print("[+] Consultando crt.sh")
     url = 'https://crt.sh/?q=%.'+domain+'&output=json'
     response = requests.get(url)
@@ -109,7 +125,7 @@ def crt(domain):
                 subs.append(sub[0])
             except Exception as e: pass #print('Error: {}'.format(e))
 
-def hackertarget(domain):
+def hackertarget():
     print("[+] Consultando hackertarget")
     headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
@@ -130,43 +146,49 @@ def hackertarget(domain):
     for x in sub:
         subs.append(x)
 
-def securitytrails(domain):
-    print("[+] Consultando securitytrails")
+def securitytrails():
     url = "https://api.securitytrails.com/v1/domain/"+domain+"/subdomains"
 
-    querystring = {"children_only":"false","include_inactive":"false"}
+    querystring = {"children_only":"false","include_inactive":"true"}
 
     # Insira sua chave de API em APIKEY
-    headers = {
-        "Accept": "application/json",
-        "APIKEY": sec_api()
-    }
-    try:    
+    try:
+        headers = {
+            "Accept": "application/json",
+            "APIKEY": sec_api()
+        }
+        
+        print("[+] Consultando securitytrails")
+        
         response = requests.request("GET", url, headers=headers, params=querystring)
         #print('\nSecurity Trails: {}'.format(response.json()))
+        
         for x in response.json()['subdomains']:
             sub = '{}.{}'.format(x,domain)
             subs.append(sub)
     except:
-        pass
+        return
 
-def passivetotal(domain):
-    print("[+] Consultando passivetotal")
+def passivetotal():
     headers = {
         'Content-Type': 'application/json',
     }
-    # Insira sua credencial de acesso como tupla = (user,pass)
-    keys = tuple(riskiq_api()) 
+ 
+    try:        
+        keys = tuple(passive_api())
+        print("[+] Consultando passivetotal")
+    except:return
 
     data = '{"query":"'+domain+'"}'
 
     response = requests.post('https://api.passivetotal.org/v2/enrichment', headers=headers, data=data, auth=keys).json()
 
     for x in response['subdomains']:
-        sub = '{}.{}'.format(x,domain)
-        subs.append(sub)
+        if not '*' in x:
+            sub = '{}.{}'.format(x,domain)
+            subs.append(sub)
 
-def dnsdb(domain):
+def dnsdb():
     print("[+] Consultando dnsdb")
     try:
         # Insira sua chave de API em X-API-Key
@@ -176,6 +198,11 @@ def dnsdb(domain):
         }
 
         response = requests.get('https://api.dnsdb.info/dnsdb/v2/regex/rrnames/.*\.'+domain+'\.$/A', headers=headers)
+        sub = re.findall('[\w\d\.\-]+\.'+domain,response.text)
+        for x in sub:
+            subs.append(x)
+        
+        response = requests.get('https://api.dnsdb.info/dnsdb/v2/regex/rrnames/.*\.'+domain+'\.$/AAAA', headers=headers)
         sub = re.findall('[\w\d\.\-]+\.'+domain,response.text)
         for x in sub:
             subs.append(x)
@@ -195,13 +222,15 @@ def dnsdb(domain):
         #for x in sub:
             #subs.append(x)
 
-    except Exception as e: print('Error: {}'.format(e))
+    except Exception as e:
+        #print('Error: {}'.format(e))
+        return
 
-def virustotal(domain):
+def virustotal():
     try:
         print('[+] Consultando Virustotal')
 
-        # Insira sua chave de API em x-apikey
+        # Insira sua chave de API como valor do x-apikey
         headers = {
         'x-apikey': virustotal_api(),
         }
@@ -224,4 +253,43 @@ def virustotal(domain):
                 subs.append(x)
     except Exception as e: 
         #print('VirusTotal: {}'.format(e))
-        pass
+        return
+
+def shodan():
+    try:    
+        print('[+] Consultando Shodan')
+
+        # Insira sua chave de API como valor do 'key'
+        params = (
+        ('key', shodan_api()),
+        )
+
+        response = requests.get('https://api.shodan.io/dns/domain/nubank.com.br', params=params).json()
+        print(response)
+
+        for sub in response['subdomains']:
+            subs.append('{}.{}'.format(sub,domain))
+
+    except Exception as e:
+        #print('Error: {}'.format(e)) 
+        return
+
+def threatcrowd():
+    try:
+        print('[+] Consultando ThreatCrowd')
+        params = {"domain": domain}
+        response =  requests.get("https://www.threatcrowd.org/searchApi/v2/domain/report/", params=params).json()
+        #print(response['subdomains'])
+        for sub in response['subdomains']:
+            subs.append(sub)
+    except:
+        return
+
+def alienvault():
+    try:
+        print('[+] Consultando AlienVault')
+        response = requests.get("https://otx.alienvault.com/api/v1/indicators/domain/"+domain+"/passive_dns").json()
+        #print(response)
+        for sub in response['passive_dns']:
+            subs.append(sub['hostname'])
+    except: return
