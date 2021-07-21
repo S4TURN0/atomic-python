1b4c01b04cc6105ae82a28e1090d35806d63c93b
S4TURN0 <wesleysampaio66@gmail.com>
2021-07-21 00:59:10 -0300

    Melhorias na randomização de APIs

--- config/keys.py
+++ config/keys.py
@@ -1,12 +1,19 @@
 import random,json
 
 def random_api(api):    
-    keys = get_api(api)
-    key = random.randrange(len(keys))
-    return keys[key]
+    try:
+        keys = get_api(api)
+        key = random.randrange(len(keys))
+        return keys[key]
+    except:
+        keys = get_api(api)['api']
+        cookies = get_api(api)['cookie']
+        key = random.randrange(len(keys))
+        cookie = random.randrange(len(keys))
+        return keys[key],cookies[cookie]
 
 def get_api(api):
-    with open('config/api_keys.json','r') as key:
+    with open('.config/api_keys.json','r') as key:
         return json.load(key)[api]
 
 def sec_api():
@@ -30,3 +37,9 @@ def shodan_api():
 
 def zoomeye_api():
     return random_api('zoomeye')
+    
+def chaos_api():
+    return random_api('chaos')
+
+def fofa_api():
+    return random_api('fofa')
