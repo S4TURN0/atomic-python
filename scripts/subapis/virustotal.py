2df6a13c483da6b7e741f8c9808c81429a87bcac
S4TURN0 <wesleysampaio66@gmail.com>
2021-08-03 22:09:26 -0300

    Melhoria na coleta e parsing de subdominios

--- scripts/subapis/virustotal.py
+++ scripts/subapis/virustotal.py
@@ -1,29 +1,20 @@
-def virustotal(domain,requests,api,re,time):
+def virustotal(domain,requests,api):
     try:
         print('[+] Consultando virustotal')
 
-        # Insira sua chave de API como valor do x-apikey
-        headers = {
-        'x-apikey': api,
-        }
+        headers = {'x-apikey': api}
         subs = []
-        regex_sub = []
-        response = requests.get('https://www.virustotal.com/api/v3/domains/'+domain+'/subdomains?limit=40', headers=headers)
-        cursor = response.json()['meta']['cursor']
-        regex_sub.append(re.findall('[\w\d\.\-]+\.'+domain, response.text))
 
+        response = requests.get('https://www.virustotal.com/api/v3/domains/'+domain+'/subdomains?limit=40', headers=headers).json()
+        for querys in response['data']:
+            subs.append(querys['id'])
         while True:
             try:
-                response = requests.get('https://www.virustotal.com/api/v3/domains/'+domain+'/subdomains?limit=40&cursor='+cursor, headers=headers)
-                regex_sub.append(re.findall('[\w\d\.\-]+\.'+domain, response.text))
-                cursor = response.json()['meta']['cursor']
-                time.sleep(5)
+                response = requests.get(response['links']['next'], headers=headers).json()
+                for querys in response['data']:
+                    subs.append(querys['id'])
             except: break
-
-        for sub in regex_sub:
-            for x in sub:
-                subs.append(x)
         return subs
-    except: 
+    except:
         print('[!] Error: '+response.json()['error']['message'],'\n')
         return
