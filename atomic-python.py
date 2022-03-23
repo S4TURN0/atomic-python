2dbf815112cc8c0b8d509cf3e5c8c08a3ee4096e
S4TURN0 <wesleysampaio66@gmail.com>
2022-03-23 02:37:39 -0300

    fix!: ajustes de exceções, correções de erros e validações de input
    refactor\!: melhoria e correções do helper (argparse)

--- atomic-python.py
+++ atomic-python.py
@@ -1,45 +1,74 @@
 #!/usr/bin/python3
-from lib.scriptslib import *
-from utils.banner import banners
-import argparse
+try:
+    from lib.scriptslib import *
+    from utils.banner import banners
+    import argparse,re
+except KeyboardInterrupt:
+    print("\n[!] Execução cancelada!")
+    exit()
 
-parse = argparse.ArgumentParser(description="Esse código serve para realizar um portscan")
+parse = argparse.ArgumentParser(prog='./atomic-python.py',allow_abbrev=True,description=banners())
 parse.add_argument('-a','--auto',action="store_true",dest="automation",help="Automatiza todo o processo de discovery, scanning e web fuzzing")
-parse.add_argument('-d','--dest',type=str,dest="destino",help="Insira o endereço de destino")
-parse.add_argument('-p',help="Insira a porta para ser escaneada")
-parse.add_argument('-s','--subs',action="store_true",dest="subdomain",help="Realizar discoberta de subdominios")
-parse.add_argument('-ps','--scan',action="store_true",dest="portscan",help="Realizar escaneamento de portas")
-parse.add_argument('-f','--fuzz',dest="fuzzing",help="Realizar um fuzzing ao encontrar portas web abertas")
-parse.add_argument('-hc',help="Para especificar os códigos HTTP que deseja esconder")
-parse.add_argument('-sc',help="Para especificar os códigos HTTP que deseja mostrar") 
-parse.add_argument('-w',type=str,help="Adicionar uma wordlist para a realização do fuzzing (OPCIONAL)")
+parse.add_argument('-d',type=str,dest="destino",help="Insira o endereço de destino (Domínio ou IPV4)")
+#parse.add_argument('-t',default=200,dest="threads",help="Modifica a quantidade de threads utilizas (padrão 200)")
+
+subs = parse.add_argument_group("Subdomain arguments")
+subs.add_argument('-s','--subs',action="store_true",dest="subdomain",help="Realizar descoberta de subdominios")
+#subs.add_argument('-sA','--subs-active',action="store_true",dest="subs_active",help="Realizar descoberta de subdominios e filtra por ativos")
+
+scan = parse.add_argument_group("Portscan arguments")
+scan.add_argument('-pS','--scan',action="store_true",dest="portscan",help="Realizar escaneamento de portas")
+scan.add_argument('-p',dest="porta",help="Insira a porta para ser escaneada")
+
+fuzz = parse.add_argument_group("Web Fuzzing arguments")
+fuzz.add_argument('-f',nargs="?",default=False,dest="fuzzing",help="Realizar descoberta de diretórios acessiveis")
+fuzz.add_argument('-hc',help="Para especificar os códigos HTTP que deseja esconder")
+fuzz.add_argument('-sc',help="Para especificar os códigos HTTP que deseja mostrar") 
+fuzz.add_argument('-w',default="utils/wordlists/common.txt",help="Adicionar uma wordlist para a realização do fuzzing (Padrão: common.txt)")
+
 args = parse.parse_args()
 
 def main():
-
-    banners()
-
     try:
         if args.destino != None:
-            domain = args.destino
-            ports = args.p
-        else:
+            if re.search('^[\w\.\-]+\.[a-z]+$',args.destino.lower()) or re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',args.destino):
+                domain = args.destino
+                ports = args.porta
+                
+                if args.automation: auto(domain,ports,args.w,args.sc,args.hc)
+                
+                elif args.subdomain and args.portscan:
+                    for domains in subdomain(domain):
+                        port_scan(domains,ports)
+
+                elif args.subdomain and args.fuzzing == None:
+                    for domains in subdomain(domain):
+                        fuzzing(domains,args.sc,args.hc,True,None,args.w)
+
+                elif args.subdomain: subdomain(domain)
+                
+                elif args.portscan: port_scan(domain,ports)
+                
+                else:
+                    parse.print_help()
+                    exit()
+            else:
+                print('[!] Destino inválido: {}\n'.format(args.destino))
+                parse.print_help()
+                exit()
+
+        elif args.fuzzing: 
             domain = None
-        
-        if args.automation: auto(domain,ports,args.w,args.sc,args.hc)
-
-        if args.subdomain: subdomain(domain)
-
-        if args.portscan == True or args.p != None: port_scan(domain,ports)
-
-        if args.fuzzing: fuzzing(domain,args.sc,args.hc,args.automation,args.fuzzing,args.w)
+            fuzzing(domain,args.sc,args.hc,args.automation,args.fuzzing,args.w)
+        else:
+            parse.print_help()
+            exit()
 
     except KeyboardInterrupt:
         print("\n[!] Execução cancelada!")
-    except Exception as e:
-        print('\n[!] Error: {}'.format(e))
+        exit()
     else:
-        print('\n[+] Execução finalizada com sucesso!')
+        print('\n[*] Execução finalizada!')
 
 if __name__ == "__main__":
     main()
