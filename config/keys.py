773b1172b8d2d6e9b76842b876521ab41359e73c
S4TURN0 <wesleysampaio66@gmail.com>
2022-03-23 02:16:44 -0300

    feat: inserção de chave para API binaryedge
    fix!: melhoria na busca e randomização das chaves

--- config/keys.py
+++ config/keys.py
@@ -1,45 +1,22 @@
-import random,json
+import random,json,os
 
-def random_api(api):    
+def random_api(api):
     try:
-        keys = get_api(api)
+        keys = get_key(api)
         key = random.randrange(len(keys))
-        return keys[key]
+        if api != 'passivetotal': 
+            return keys[key]
+        else:
+            user,passwd = keys[key]
+            return user,passwd
     except:
-        keys = get_api(api)['api']
-        cookies = get_api(api)['cookie']
+        keys = get_key(api)['api']
+        cookies = get_key(api)['cookie']
         key = random.randrange(len(keys))
         cookie = random.randrange(len(keys))
         return keys[key],cookies[cookie]
 
-def get_api(api):
-    with open('config/api_keys.json','r') as key:
+def get_key(api):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(dir_path+'/api_keys.json','r') as key:
         return json.load(key)[api]
-
-def sec_api():
-    return random_api('sectrails')
-
-def cert_api():
-    return random_api('certspotter')
-
-def dnsdb_api():
-    return random_api('dnsdb')
-
-def virus_api():
-    return random_api('virustotal')
-
-def passive_api():
-    user,passwd = random_api('passivetotal')
-    return user,passwd
-
-def shodan_api():
-    return random_api('shodan')
-
-def zoomeye_api():
-    return random_api('zoomeye')
-    
-def chaos_api():
-    return random_api('chaos')
-
-def fofa_api():
-    return random_api('fofa')
