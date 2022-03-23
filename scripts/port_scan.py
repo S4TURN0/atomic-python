e9b9e868ec8eac6a8e029a86eb20ee40b756f0d7
S4TURN0 <wesleysampaio66@gmail.com>
2022-03-23 02:31:53 -0300

    fix: ajuste no tratamento de exceções

--- scripts/port_scan.py
+++ scripts/port_scan.py
@@ -16,44 +16,48 @@ def get_ip_domain(domain):
 
 # Função inicial para gerenciar as portas e iniciar o escaneamento
 def port_scan(domain,ports):
-    if ports == None:              
-        # Montando a lista de portas padrões
-        ports = []
-        for x in range(1,1025): 
-            ports.append(x)
-        return connect_ports(domain,ports)
+    try:
+        if ports == None:         
+            # Montando a lista de portas padrões
+            ports = []
+            for x in range(1,1025): 
+                ports.append(x)
+            return connect_ports(domain,ports)
 
-    elif '-' in ports:
-        # Montando a lista de portas padrões
-        start_port, end_port = ports.split("-")
-        start_port, end_port = int(start_port), int(end_port)
-        ports = [ p for p in range(start_port, end_port+1)]             
-        return connect_ports(domain,ports)
+        elif '-' in ports:
+            # Montando a lista de portas padrões
+            start_port, end_port = ports.split("-")
+            start_port, end_port = int(start_port), int(end_port)
+            ports = [ p for p in range(start_port, end_port+1)]             
+            return connect_ports(domain,ports)
 
-    elif ',' in ports:
-        # Montando a lista de portas padrões    
-        ports = ports.split(",")
-        ports = [int(i) for i in ports]
-        return connect_ports(domain,ports)
-            
-    else: 
-        ports = [int(ports)]
-        return connect_ports(domain,ports)    
-
+        elif ',' in ports:
+            # Montando a lista de portas padrões    
+            ports = ports.split(",")
+            ports = [int(i) for i in ports]
+            return connect_ports(domain,ports)
+                
+        else: 
+            ports = [int(ports)]
+            return connect_ports(domain,ports)    
+    except ValueError:
+        print("[!] Error: Invalid port value")
+        
 # Função para realizar o portscan
 def connect_ports(domain,ports):
-    print("[*] Inciando o portscan no destino: ",domain,"\n")
+    print("\n[*] Inciando o portscan no destino: ",domain,"\n")
     port_found = []
     try:
         for port in ports:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(1)
             connect = sock.connect_ex((domain,port))
-
             if connect == 0:
                 port_found.append(port)
                 print("Conexão aberta:", getServiceName(port,'tcp'))        
                 sock.close()
+        if len(port_found) == 0:
+            print("[!] Não foi possível encontrar portas abertas para este destino!")
     except socket.gaierror:
         print("[!] Não foi possivel se conectar ao dominio:",domain,"\n")
     return port_found
